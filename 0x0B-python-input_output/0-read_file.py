63e3c722cea6eef39cdbc0d6aad4fde2d33a2a26
Carlaestelle <carlamwasumbi25@gmail.com>
2023-04-12 14:07:55 -0700

    Read file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-"""Defines a function that reads a text file"""
+"""Defines a function that reads a text file."""
 
 
 def read_file(filename=""):
-    """Prints the content of UTF8 file to stdout"""
+    """Prints the content of UTF8 file to stdout."""
     with open(filename, encoding = 'utf-8') as f:
-        print(f.read())
+        print(f.read(), end="")
